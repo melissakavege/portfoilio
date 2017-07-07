04d570c40eb84846070c3af3a5e09ef23ad555ee
melissakavege <mkavege53@gmail.com>
2017-07-07 14:43:13 -0400

    Create cocos_Day.py

---
+++ cocos_Day.py
@@ -0,0 +1,32 @@
+start = '''You are walking your dog Coco and she has to go pee but you have to
+get to work or you are going to be late for the fifth time and if you are late
+on the the fifth time you get fired'''
+print(start)
+user_input = input( '''type home to go to your house and change into your
+work attire or type wait to keep walking your dog''')
+if user_input =="home":
+    print('''you go hohme and your dog pees on the floor before you leave. Coco is now stained''')
+    print('''since you were running late you had to get on a later bus, where
+    you see your old teacher from high school and she tells you that ther is
+        a job opening at a start up technology company in which she is the
+        co-founder she proceeds to offer you the job''')
+    usr_input = input ('''do you accpect the job''')
+    if usr_input == "yes":
+        print('''you work hard and after a month you get a raise and you get promted
+    to lead designer and you are able to affair that new car you have been hoping for''')
+    if usr_input =="no":
+        print('''you ran to your job and you were one hour late and your boss gives
+    your title as manager to someone new and now you are getting paid one hundred dollars less''')
+if user_input == "wait":
+    print('''you wait for your dog to pee and you get back home, change, and get
+     to work a minute before you are late''')
+    print('''you come back home and your dog just got groomed and you set-up for
+    your family to come and they see that you place is nice and that
+    you are able to take care of your self and they said that they will give you some money''')
+    us_input = input("will you take the money")
+    if us_input =="yes":
+        print('''your parnets gives you 500 dollars each month for a year but
+        you didn't save the money and you spent it on things that you did not
+        need and then you had to move back with your parnets and you have to pay
+        rent each month and you are upset now because you were childish and didn't
+        act like an adult and didn't be resonible.''')
